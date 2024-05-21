2d6166982e952edac5d89294a03b86a95918f9d6
rnarule <rnarule@gmail.com>
2024-05-21 14:37:05 +0530

    PROJECT_SETUP

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Generative AL Projects",
+    version="0.0.0",
+    author="rnarule",
+    author_email="rnarule@gmail.com",
+    packages=find_packages()
+)
